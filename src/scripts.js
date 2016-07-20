be0f4fca3bf7c4062f0a4c6151df86c00188379b
Michael Krukar <krukar@gmail.com>
2016-07-19 21:59:53 -0400

    Second grid added and work and menu toggles added

---
+++ src/scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('jsMenuToggle').addEventListener('click', function(){
+	document.getElementById('jsMenu').classList.toggle('open');
+});
+
+	document.getElementById('jsMainToggle').addEventListener('click', function(){
+	document.getElementById('jsMain').classList.toggle('open');
+});
