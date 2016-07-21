0771c4ed0eb9e57c9252de040ae90c33f49753dd
Michael Krukar <krukar@gmail.com>
2016-07-20 22:22:11 -0400

    Header and footer responsive complete. Added toggle buttons that need to be optimized

--- src/scripts.js
+++ src/scripts.js
@@ -1,7 +1,31 @@
-document.getElementById('jsMenuToggle').addEventListener('click', function(){
-	document.getElementById('jsMenu').classList.toggle('open');
+document.getElementById('jsMainToggle').addEventListener('click', function(){
+	document.getElementById('jsMain').classList.toggle('open');
 });
 
-	document.getElementById('jsMainToggle').addEventListener('click', function(){
-	document.getElementById('jsMain').classList.toggle('open');
+document.getElementById('jsWebToggle').addEventListener('click', function(){
+	var hide = document.querySelectorAll('.harley, .kaleb, .krukar, .liquid, .wd40, .barry, .meundies');
+	for(item of hide){
+		item.classList.toggle('hide');
+	}
+});
+
+document.getElementById('jsArtToggle').addEventListener('click', function(){
+	var hide = document.querySelectorAll('.makeful, .media, .netflix, .radio, .schedulizer, .trace, .uber, .liquid, .wd40, .barry, .meundies');
+	for(item of hide){
+		item.classList.toggle('hide');
+	}
+});
+
+document.getElementById('jsVideoToggle').addEventListener('click', function(){
+	var hide = document.querySelectorAll('.makeful, .media, .netflix, .radio, .schedulizer, .trace, .uber, .harley, .kaleb, .krukar, .barry, .meundies');
+	for(item of hide){
+		item.classList.toggle('hide');
+	}
+});
+
+document.getElementById('jsPhotoToggle').addEventListener('click', function(){
+	var hide = document.querySelectorAll('.makeful, .media, .netflix, .radio, .schedulizer, .trace, .uber, .harley, .kaleb, .krukar, .liquid, .wd40');
+	for(item of hide){
+		item.classList.toggle('hide');
+	}
 });
